b22fa03cc2da0c58eca9b420c7ae2a984e088a8b
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-10-13 17:09:43 +0300

    Начинаем разбивку маркапа на компоненты

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import Main from './pages/main/main';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <Main></Main>
   </React.StrictMode>
 );
