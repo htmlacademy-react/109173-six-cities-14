ec7201cd9b112381fe6c28b4c27dfe8382d9e84b
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-10-16 16:59:37 +0300

    Исправляем структурные ошибки. Дорезаем компоненты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import Main from '../../../pages/main/main';
+import Main from '../../pages/main/main';
 import { AppProps } from './app.props';
 
 export default function App({ offersCount }: AppProps): JSX.Element {
