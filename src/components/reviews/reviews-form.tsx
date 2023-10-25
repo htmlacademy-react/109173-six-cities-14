9380c2753e1101d564e7550b95a3e62b96d391a9
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-10-25 16:54:24 +0300

    Исправление ошибок типизации и рефакторинг компонента review-form

--- src/components/reviews/reviews-form.tsx
+++ src/components/reviews/reviews-form.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { Fragment, FormEvent, useState } from 'react';
 import { REVIEW_TEXT_MIN_LENGTH } from '../../const';
 
 type StartRatingProps = {
@@ -12,20 +12,20 @@ function StarsRating({ rating }: StartRatingProps): React.ReactElement {
     <div className="reviews__rating-form form__rating">
       {
         stars.map((star) => (
-          <>
+          <Fragment key={ star } >
             <input
               className="form__rating-input visually-hidden"
               name="rating" defaultValue={ star }
               id={`${ star }-stars`}
               type="radio"
-              checked={(star <= rating) ? true : false}
+              defaultChecked={star <= rating}
             />
             <label htmlFor={`${ star }-stars`} className="reviews__rating-label form__rating-label" title="perfect">
               <svg className="form__star-image" width={ 37 } height={ 33 }>
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
-          </>
+          </Fragment>
         ))
       }
     </div>
@@ -45,7 +45,7 @@ export default function ReviewsForm(): React.ReactNode {
   const [userReview, setUserReview] = useState(reviewState);
 
   function formSubmitHandler(evt: FormEvent<HTMLFormElement>) {
-    const target = evt.target;
+    const target = evt.target as HTMLFormElement;
     const formData = new FormData(target); // TODO: Не совсем понятно, что тут не так с таргетом?
 
     const review: ReviewProps = {
