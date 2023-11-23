0ad2580226dfd4fc8742ed71932486c707c114d9
Dmitriy Nosikov <deimon1993rf2@yandex.ru>
2023-11-23 16:34:05 +0300

    Добавлен компонент пустой страницы. Поправлена логика проверки загрузки офферов

--- src/store/slices/offers-data-process/offers-data-process.ts
+++ src/store/slices/offers-data-process/offers-data-process.ts
@@ -2,9 +2,11 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { NAMESPACE } from '../../../const';
 import { OffersDataProcess } from '../../../types/state';
 import { Offers } from '../../../types/offer';
+import { fetchOffersAction } from '../../api-action';
 
 const initialState: OffersDataProcess = {
   offers: [],
+  isOffersLoading: false,
 };
 
 export const offersDataProcess = createSlice({
@@ -14,7 +16,22 @@ export const offersDataProcess = createSlice({
     loadOffersAction: (state, action: PayloadAction<{ offers: Offers }>) => {
       state.offers = action.payload.offers;
     },
+    setOffersLoadingStatus: (state, action: PayloadAction<boolean>) => {
+      state.isOffersLoading = action.payload;
+    }
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchOffersAction.pending, (state) => {
+        state.isOffersLoading = true;
+      })
+      .addCase(fetchOffersAction.fulfilled, (state) => {
+        state.isOffersLoading = false;
+      })
+      .addCase(fetchOffersAction.rejected, (state) => {
+        state.isOffersLoading = false;
+      });
+  }
 });
 
-export const { loadOffersAction } = offersDataProcess.actions;
+export const { loadOffersAction, setOffersLoadingStatus } = offersDataProcess.actions;
