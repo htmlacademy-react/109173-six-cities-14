1d925ac65c279b0f136ae982464c71fbd07a320c
Deimon <deimon1993rf2@yandex.ru>
2023-12-02 23:02:38 +0300

    Продолжаем написание тестов для хуков. WIP

--- src/hooks/useMap.test.tsx
+++ src/hooks/useMap.test.tsx
@@ -1,18 +1,15 @@
-import { render, renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { makeMockCity } from '../utils/mock';
-import { useRef } from 'react';
 import useMap from './useMap';
 
 describe('[Hook: useMap]:', () => {
   it('Should return "Map"', () => {
     const cityInfo = makeMockCity();
-    const mapRef = useRef(null);
-    const mapContainer = <section ref={ mapRef }></section>;
+    const mapRef = { current: null };
 
     const { result } = renderHook(() => useMap({ cityInfo, mapRef }));
-    render(mapContainer);
 
-    console.log('--------------------- MAP: ', result.current);
+    console.log('--------------------- MAP: ', result);
 
     // expect(typeof result.current).toBe('function');
   });
